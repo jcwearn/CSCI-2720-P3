c1868a8963f80c0e29058b96f3606a73ee1cef37
Jackson Wearn <jacksonwearn@gmail.com>
2014-04-19 17:56:07 -0400

    basic huffman tree functionality.  working on exporting lib

--- string_repr.cpp
+++ string_repr.cpp
@@ -2,32 +2,118 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 using namespace std;
 
 struct table {
-    int ascii;
-    int value;
+  int ascii;
+  int frequency; //value;
+};
+
+struct library {
+  int ascii;
+  int bit_string;
 };
 
 struct node {
-  node * left_child;
-  node * right_child;
+  node * left;
+  node * right;
   int frequency;
-  int value;
+  int bit_value;
   int ascii;
+  string bit_string;
 };
 
 //function to sort table<vector>
 bool compare_table(const table& a, const table& b) {
-    return a.value < b.value;
+    return a.frequency < b.frequency;
 }
+
+/*
+bool compare_node(const node& a, const node& b) {
+  return a->frequency < b->frequency;
+  }*/
 
 class String_repr {
 private:
   vector<table> freq;
+  vector<library> libr;
   vector<node*> trees;
   int table_size;
+  int tree_size;
   node * root;
+
+  void printInOrderPrivate(node * root){
+    if(root != NULL) {
+      if(root->left != NULL) {
+	printInOrderPrivate(root->left);
+      }
+      
+      if(root->ascii != -1) {
+	cout << "Frequency: " << root->frequency << endl;
+      }
+      
+      if(root->right != NULL) {
+	printInOrderPrivate(root->right);
+      }
+    }
+    else {
+      cout << "Tree is empty\n";
+    }
+  }
+
+  void set_bit_value_private(node * root) {
+    if(root != NULL) {
+      if(root->left != NULL) {
+	root->left->bit_value = 0;
+	set_bit_value_private(root->left);
+      }
+      if(root->right != NULL) {
+	root->right->bit_value = 1;
+	set_bit_value_private(root->right);
+      } 
+    }
+  }
+
+  void build_bit_strings_private(node * root) {
+    string s;
+    int i;
+    char c;
+    std::ostringstream ss;
+    
+    if(root != NULL) {
+      if(root->left != NULL) {
+	i = root->left->bit_value;
+	ss << i;
+	s = ss.str();
+	root->left->bit_string = root->bit_string;
+	root->left->bit_string += s; 
+	build_bit_strings_private(root->left);
+      }
+      
+      if(root->ascii != -1) {
+	//library lib;
+
+	//root->bit_string;
+	//lib = {}
+	//library.push_back();
+	c = root->ascii;
+	cout << "Frequency of " << c << ": " << root->frequency << " Bit string: " << root->bit_string << endl;
+      }
+      
+      if(root->right != NULL) {
+	i = root->right->bit_value;
+	ss << i;
+	s = ss.str();
+	root->right->bit_string = root->bit_string;
+	root->right->bit_string += s; 
+	build_bit_strings_private(root->right);
+      }
+    }
+    else {
+      cout << "Tree is empty\n";
+    }
+  }
   
 public:
   String_repr();
@@ -35,41 +121,89 @@ public:
 
   void init_nodes() {
     int i;
+    node * n;
+    
     for(i = 0; i < table_size; i++) {
-      node * n;// = {NULL,NULL,freq[i].value,0,freq[i].ascii};
-      n->left_child = NULL;
-      n->right_child = NULL;
-      n->frequency = freq[i].value;
-      n->value = 0;
+      n = new node;
+      n->left = NULL;
+      n->right = NULL;
+      n->frequency = freq[i].frequency;
+      n->bit_value = 0;
       n->ascii = freq[i].ascii;
       trees.push_back(n);
     }
+    tree_size = trees.size();
   }
 
-  void makeCode() {
-    int vector_size = trees.size();
-    
-    while(vector_size > 1) {
-      int sum = trees.at(0)->frequency + trees.at(1)->frequency; // .frequency + trees.at(1).frequency;
-      node * n;// = {&trees.at(0), &trees.at(1), sum, 0, -1};
-      n->left_child = trees.at(0);
-      n->right_child = trees.at(1);
-      n->frequency = sum;
-      n->value = 0;
-      n->ascii = -1;
-      trees.at(1)->value = 1; //.value = 1;
+  void build_tree() {
+    int vector_size = tree_size;
+    int sum;
+    int i;
+    node * n;
 
-      trees.erase(trees.begin(),trees.begin()+1);
-      trees.push_back(n);
+    while(vector_size > 1) {
+      sum = trees[0]->frequency + trees[1]->frequency;
+      n = new node;
+
+      if((trees[0]->ascii == -1) && (trees[1]->ascii == -1)) {
+	n->left = trees[1];
+	n->right = trees[0];
+      }
+      else if(trees[0]->ascii == -1) {
+	n->left = trees[1];
+	n->right = trees[0];
+      }
+      else if(trees[1]->ascii == -1) {
+	n->left = trees[0];
+	n->right = trees[1];
+      }
+      else {
+	n->left = trees[1];
+	n->right = trees[0];
+      }
+
+      n->frequency = sum;
+      n->bit_value = 0;
+      n->ascii = -1;
+
+      trees.erase(trees.begin(),trees.begin()+2);
+
       vector_size--;
+
+      i = 0;
+      while((trees[i]->frequency) < (n->frequency)) {
+	i++;
+	if(i >= vector_size)
+	  break;
+      }
+      
+
+      if(i >= vector_size) 
+	trees.push_back(n);
+      else
+	trees.insert(trees.begin()+i,n);
+      
       root = n;
-    }
+    }    
+  }
+
+  void set_bit_value() {
+    set_bit_value_private(root);
+  }
+
+  void printInOrder() {
+    printInOrderPrivate(root);
+  }
+
+  void build_bit_strings() {
+    build_bit_strings_private(root);
   }
 };
 
 String_repr::String_repr(vector<table> t, int size) {
   freq = t;
   table_size = size;
+  root = NULL;
 }
 
 class Freq_table {
@@ -89,7 +223,7 @@ public:
 
     for(i = 0; i < table_size; i++) {
       c = freq_resized[i].ascii;
-      cout << "Freq of " << c << " : " << freq_resized[i].value << endl; 
+      cout << "Freq of " << c << " : " << freq_resized[i].frequency << endl; 
     }
   }
 
@@ -181,7 +315,6 @@ int main() {
   while(still_running) {
     cout << ">> ";
     cin >> user_input;
-    //user_input = cin.get();
     user_input_aschar = user_input.at(0);
     
     
@@ -198,7 +331,10 @@ int main() {
     
     switch (user_case) {
     case 1:
-      repr->makeCode();
+      repr->build_tree();
+      repr->set_bit_value();
+      repr->build_bit_strings();
+      //repr->printInOrder();
       still_running = false;
       break;
 
